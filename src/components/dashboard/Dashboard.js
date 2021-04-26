71852512326a222a57e8e0f54d250d4062a965af
LironTexel <liron@texel.live>
2021-04-26 20:02:04 +0300

    notifications and fb

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Notifications from "./Notifications";
 import BrandsList from "../Brands/BrandsList";
-// import { connect } from "react-redux";
 import { useSelector } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { Redirect } from 'react-router-dom';
 
 const Dashboard = () => {
     const brands = useSelector((state) => state?.firestore?.ordered?.brands);
+    const notifications = useSelector((state) => state?.firestore?.ordered?.notifications);
     const auth = useSelector((state) => state?.firebase?.auth);
 
     if (!auth.uid) return <Redirect to='/login'/>;
     else return (
         <div className="dashboard container">
-            <Notifications/>
+            <Notifications notifications={ notifications }/>
             <BrandsList brands={ brands }/>
         </div>
     )
@@ -21,4 +21,7 @@ const Dashboard = () => {
 
 // connect causing an error. known issue with react-redux-firebase
 // https://github.com/prescottprue/react-redux-firebase/issues/1059
-export default firestoreConnect([{ collection: 'brands' }])(Dashboard);
+export default firestoreConnect([
+    { collection: 'brands', orderBy: ['createdAt', 'desc'] },
+    { collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
+    ])(Dashboard);
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,27 @@
 import React from "react";
+// import {Link} from "react-router-dom";
+// import BrandSummary from "../Brands/BrandSummary";
+import moment from "moment";
 
-const Notifications = () => {
+const Notifications = ({ notifications }) => {
     return (
-        <div className={"notifications"}>
-            Notifications
+        <div className={"section"}>
+            { notifications && notifications.map( notification => {
+                    return (
+                        <div>
+                            <p key={ notification.id }>{notification.content} - {notification.user}</p>
+                            <div>date - { moment(notification.time.toDate()).fromNow() }</div>
+                        </div>
+                    )
+                })
+            }
+            {/*<div className="card">*/}
+            {/*    <ul className="notifications">*/}
+            {/*        <li>notification</li>*/}
+            {/*        <li>notification</li>*/}
+            {/*        <li>notification</li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
         </div>
     )
 }
